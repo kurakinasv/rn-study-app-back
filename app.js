b204710ec075ed89c55dadf9f52c3e6d32fe9cbe
kurakinasv <svetakurakina2002@gmail.com>
2023-05-19 14:58:35 +0300

    server setup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const initRoutes = require('./routes');
+
+const app = express();
+
+app.use(express.json({ extended: true }));
+
+initRoutes(app);
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+  } catch (e) {
+    console.log('Server Error', e.message);
+    process.exit(1);
+  }
+};
+
+start();
